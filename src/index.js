7a896db0a586c8b20113f932faf8aef9f68a9780
Mauricio Garcia <maupessoagarcia@yahoo.com>
2023-04-22 04:31:56 +0200

    Created first robots

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
-import Hello from "./Hello";
+import Card from "./Card";
 import reportWebVitals from "./reportWebVitals";
+import "tachyons";
+import { robots } from "./robots";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Hello greetings="Hello React Ninja" />
+    <Card id={robots[0].id} name={robots[0].name} email={robots[0].name} />
+    <Card id={robots[1].id} name={robots[1].name} email={robots[1].name} />
+    <Card id={robots[2].id} name={robots[2].name} email={robots[2].name} />
   </React.StrictMode>
 );
 
