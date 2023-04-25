7775845b09e14a869c8d64a519e9034fd5d62045
Mauricio Garcia <maupessoagarcia@yahoo.com>
2023-04-25 04:28:15 +0200

    Failed gitignore, deleted files

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Cardlist from "./Cardlist";
 import { robots } from "./robots";
 import SearchBox from "./SearchBox";
+import "./App.css";
 
 class App extends Component {
   constructor() {
